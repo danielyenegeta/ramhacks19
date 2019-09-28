090b30d981a91b59840b7432dbc4da873719aacb
yenegeta <danielyenegeta35@gmail.com>
2019-09-28 14:53:25 -0400

    routing

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 app.config["MONGO_URI"] = "mongodb+srv://ramhacks:ramhackspassword@ramhacks19-shg2d.mongodb.net/test?retryWrites=true&w=majority"
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template("base.html")
+
+@app.route('/howitworks')
+def howitworks():
+    return render_template("howitworks.html")
+
+@app.route('/whomadethis')
+def whomadethis():
+    return render_template("whomadethis.html")

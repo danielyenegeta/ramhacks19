eee3fed8c37a5444ac2f84c2946e7ecf665fba17
yenegeta <danielyenegeta35@gmail.com>
2019-09-28 14:26:16 -0400

    db stuff

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb+srv://ramhacks:ramhackspassword@ramhacks19-shg2d.mongodb.net/test?retryWrites=true&w=majority"
 @app.route('/')
 def hello():
     return "Hello World!"

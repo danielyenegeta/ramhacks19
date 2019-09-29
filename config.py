e6bf870b328239b3b3d84884b4921084616b6853
yenegeta <danielyenegeta35@gmail.com>
2019-09-29 01:34:59 -0400

    changes

---
+++ config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ramhacks:ramhackspassword@ramhacks.c2wd89rm1pqs.us-east-1.rds.amazonaws.com:3306/ramhacks'
+SECRET_KEY = 'hU+Hn/KJUkKjy5KF6xY4loINbJkVIRVmQwKdhMoX'
